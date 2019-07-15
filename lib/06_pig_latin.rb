301baa3ec6ea619f4f24a133c618f20d235c299a
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-15 23:06:16 +0200

    update of file 06

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,55 +1,34 @@
-#def add_ending(string,index=0)
- #	string << string[index]
-#end
-
-#def remove(string,index=0)
-#	 string[index] = ""
-#end
-	
-
-
-#def translate(string, index=0)
-
-	
-	
-#	if  string[1, 2].match(/[^aeiou]/) && string[0].match(/[^aeiou]/)
-		#string[index].match(/[^aeiou]/)
-#		string << string[index]
-#		string[index] = ""
-#		return string + "ay"
-
-#	else return string + "ay"
-#	end
-
-#end
-
 def translate(sentence)
-  sentence = sentence.split
-  sentence.each_with_index do |word, index|
-    word = word.chars
-    while
 
-      letter = word[0]
+sentence = sentence.split                        # .split permet de séparer chaque mot d'une phrase et de les mettre dans un array.
+sentence.each_with_index do |word, index|        # énumère chaque mot de la phrase sans en changer l'ordre.
+word = word.chars                                # permet de prendre en compte chaque lettre de mots de manière séparée.
 
-      if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "y" || letter == "u"
-        break
-      else
+  while
 
-        if letter == "q" && word[1] == "u"
+  first_letter = word[0]                         #la première lettre du mot correspond à la lettre en position 0.
+
+    if first_letter.match(/[aeiou]/)           # Regex permettant de détecter les voyelles (a e i o u ) dans un string.
+    break                                    # si la première lettre est une voyelle, la boucle s'arrête.
+      
+    else                                       
+
+    if first_letter == "q" && word[1] == "u" # si "q" est la premiere lettre du mot et que la deuxieme lettre est un "u",
           2.times do
-            word.delete_at(0)
+          word.delete_at(0)                    # on supprime à deux reprises la lettre en première position (on supprime donc d'abord le "q" puis le "u")
           end
-          word.push("qu")
+          word.push("qu")                        # ensuite grâce à .push on rajoute "qu" à la fin du mot.
         else
-          word.delete_at(0)
-          word.push(letter)
+          word.push(first_letter)               # si la première lettre est une consonne, et que les deux premières lettres sont différentes de "qu", 
+          word.delete_at(0)                      # alors on ajoute cette consonne à la fin du mot puis on supprime la première lettre du mot .
+                                                
         end
 
       end
 
     end
 
-    word.push("ay")
+word.push("ay") 
     word = word.join("")
     sentence[index] = word
   end
